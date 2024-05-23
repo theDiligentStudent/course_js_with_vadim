94a5ec65d84bcb09a3a92b6d96ea59691206333d
theDiligentStudent <ivanrenkin14@gmail.com>
2024-05-23 15:03:26 +0300

    23/05/24 ---- смотрю 2 Ур. Вадима по JS -- посмотрел примерно Половину

---
+++ remember_myself/main.js
@@ -0,0 +1,11 @@
+// 23/05/24
+
+const friends = ', Friends!';
+console.log(`Hello${friends}`);
+
+console.log(`Hi,
+        everybody,   
+                    friends!`)
+
+console.log(3 ** 3);
+console.log(5 % 3);
