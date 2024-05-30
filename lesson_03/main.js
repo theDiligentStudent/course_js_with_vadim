c930b6866253c0c2433715da0689411ef54089c7
theDiligentStudent <ivanrenkin14@gmail.com>
2024-05-30 20:36:32 +0300

    30/05/24 ---- 3-ий Ур. Начал смотреть. Посмотрел 3 минуты. Создал для этого Ур.-а - специальную папку и html, js-файлы

---
+++ lesson_03/main.js
@@ -0,0 +1,43 @@
+// 30/05/24
+
+// Начинаю учить 3-ий Ур. Вадима
+
+// window.alert('j');
+
+// Опы. ср.
+
+// a > b      a < b
+console.log(10 > 1); // true
+console.log(10 < 1); // false
+
+// a >= b      a <= b
+// Больше/равно    меньше или равно
+console.log(10 >= 1); // true
+console.log(10 <= 1); // false
+console.log(1 <= 10); // true
+
+// НЕ строгое равно
+//    ==
+
+// Строгое равно
+//    ===
+console.log(70 === 70); // true
+console.log(7 === 70); // false
+
+// a != b    НЕ равно
+// если a НЕ равно b (то)
+console.log(6 != 6); // false   6 НЕ равно 6? ---- НЕ правда (НЕТ)
+console.log(16 != 6); // true    16 НЕ равно 6? ---- Правда (ДА)
+
+// Comparison of data types
+
+console.log(7 === 'кошмары');
+
+console.log('10' === 1);
+console.log('10' > 1); // 10 > 1  // false
+console.log('10' < 1);
+console.log('10' < 10);
+
+// КАК работают СТРОГОЕ и НЕСТРОГОЕ Равенства
+console.log('10' == 10); // true
+console.log('10' === 10); // false
